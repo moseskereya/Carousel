2fd512b5ddb7b9910fac50b4df515003a8500564
MOSES <MOSESLAZARO2019@outlook.com>
2019-10-17 03:25:51 +0300

    Updates

---
+++ precache-manifest.75846beba505f6c9bebaf1ff89dfd417.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "348e1326af2709e27368846e1cce6da4",
+    "url": "/Carousel/index.html"
+  },
+  {
+    "revision": "616015190829282eb76d",
+    "url": "/Carousel/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "8f244ee797f9ac04602c",
+    "url": "/Carousel/static/css/main.52902cbf.chunk.css"
+  },
+  {
+    "revision": "616015190829282eb76d",
+    "url": "/Carousel/static/js/2.7a1a7280.chunk.js"
+  },
+  {
+    "revision": "8f244ee797f9ac04602c",
+    "url": "/Carousel/static/js/main.841d3ac5.chunk.js"
+  },
+  {
+    "revision": "81cfce1c40b0cdfa234c",
+    "url": "/Carousel/static/js/runtime-main.3f4eef56.js"
+  }
+]);
